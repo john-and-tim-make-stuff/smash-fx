3798284532eee887f1a49e36a112c1be089b1cdf
John McElmurray <john@outlook.com>
2019-02-28 00:23:23 -0800

    Lots of tuning
    - Using the brighter yellow gradient in mat_batman_bright.mat
    - Updating pivots and stretch lengths of the ps_batman particle systems & made a prefab from them
    - Updated ps_sparkle_starburst.prefab with new default sizes and using Tim's meshes
    - Tuned the particles in-scene
    - Fixed wrapping on all text_batman textures
    - Made new scripts to force the batman particles to face the camera

---
+++ smash-fx/Assets/Scripts/Editor/ParticlesFaceCameraEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ParticlesFaceCamera))]
+public class ParticlesFaceCameraEditor : Editor
+{
+    [DrawGizmo(GizmoType.Selected | GizmoType.NonSelected | GizmoType.Pickable)]
+    static void DrawGizmoForActionableItemSpawner(ParticlesFaceCamera faceCam, GizmoType gizmoType)
+    {
+        faceCam.Update();
+    }
+}
---
+++ smash-fx/Assets/Scripts/ParticlesFaceCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParticlesFaceCamera : MonoBehaviour
+{
+    public void Update()
+    {
+        Transform lookAt = null;
+
+        if (Application.isPlaying)
+        {
+            if (Camera.main != null)
+            {
+                lookAt = Camera.main.transform;
+            }
+        }
+        else
+        {
+#if UNITY_EDITOR
+            var view = SceneView.currentDrawingSceneView;
+            if (view != null)
+            {
+                lookAt = view.camera.transform;
+            }
+#endif
+        }
+
+        if (lookAt != null)
+        {
+            transform.LookAt(transform.position + lookAt.transform.rotation * Vector3.forward, lookAt.transform.rotation * Vector3.up);
+        }
+    }
+}
